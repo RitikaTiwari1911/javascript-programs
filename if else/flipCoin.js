01f028038c832ade310e90c128dff85f1549f049
RitikaTiwari1911 <spk2ritika1911@gmail.com>
2021-05-17 23:27:33 +0530

    ADDED: Flipping a coin to get heads or tails

---
+++ if else/flipCoin.js
@@ -0,0 +1,12 @@
+//Constants
+const HEADS = 1;
+
+flipCoin = Math.floor(Math.random() * 2);
+console.log(flipCoin);
+
+if(flipCoin == HEADS){
+    console.log("Heads");
+}
+else{
+    console.log("Tails")
+}
