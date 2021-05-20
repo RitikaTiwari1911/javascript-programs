04d184440cb80d3dd0666ae78b8d956a78567031
RitikaTiwari1911 <80441401+RitikaTiwari1911@users.noreply.github.com>
2021-05-20 08:06:05 +0530

    ADDED: Read number and return unit place

---
+++ switchCase.js/unitNumber.js
@@ -0,0 +1,37 @@
+
+const ps = require("prompt-sync");
+const prompt = ps();
+
+let num = prompt("Enter the unit number: ");
+num = Number(num);
+
+switch(num){
+
+    case 1:
+        console.log("unit");
+        break;
+
+    case 10:
+        console.log("Ten");
+        break;
+
+    case 100:
+        console.log("Hundred");
+        break;
+
+    case 1000:
+        console.log("Thousand");
+        break;
+
+    case 10000:
+        console.log("Ten Thousand");
+        break;
+
+    case 100000:
+        console.log("Lakh");
+        break;
+
+    default:
+        console.log("Enter the correct number!!");
+        break;
+}
